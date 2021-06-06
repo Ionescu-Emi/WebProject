2ea043dbd189cf8f0f65dea22f4f7a606661568e
Filioreanu Marius <62004975+MariusF233@users.noreply.github.com>
2021-06-06 17:24:32 +0300

    Add files via upload

--- mvc/public/html/submit/admin_page.sub.php
+++ mvc/public/html/submit/admin_page.sub.php
@@ -46,6 +46,37 @@ if(nameExists($conn,$nameDelete)==false){
 deleteUser($conn,$nameDelete);
 
 }
+ else if(isset($_POST["download_all_users"])){
+    require_once 'dbh.sub.php';
+   require_once 'functions.sub.php';
+
+    $sql="SELECT usersId,usersName,usersEmail FROM users;";
+    if(!$result=mysqli_query($conn,$sql)){
+        exit(mysqli_error($conn));
+    }
+    $users=array();
+    if(mysqli_num_rows($result)>0){
+    
+        while($row=mysqli_fetch_assoc($result)){
+            $users[]=$row;
+        }
+    }
+    header('Content-Type:text/csv;charset=utf-8');
+    header('Content-Disposition:attachment; filename=Users.csv','w');
+    $output=fopen('php://output','w');
+    fputcsv($output,array('Id','Name','Email'));
+    
+    if(count($users)>0){
+        foreach($users as $row){
+            fputcsv($output,$row);
+        }
+    }
+    
+    
+
+ }
+ 
+ 
  else if(isset($_POST["submit_delete_visit"])){
     //echo 'ok';
     $DeleteVisitId=$_POST["DeleteVisitId"];
@@ -68,6 +99,102 @@ deleteUser($conn,$nameDelete);
  deleteVisit($conn,$DeleteVisitId);
  
  }
+ else if(isset($_POST["download_all_visits"])){
+    require_once 'dbh.sub.php';
+   require_once 'functions.sub.php';
+
+    $sql="SELECT * FROM visits;";
+    if(!$result=mysqli_query($conn,$sql)){
+        exit(mysqli_error($conn));
+    }
+    $visits=array();
+    if(mysqli_num_rows($result)>0){
+    
+        while($row=mysqli_fetch_assoc($result)){
+            $visits[]=$row;
+        }
+    }
+    header('Content-Type:text/csv;charset=utf-8');
+    header('Content-Disposition:attachment; filename=Visits.csv','w');
+    $output=fopen('php://output','w');
+    fputcsv($output,array('name_visitor','detained_name','relationship','nature','duration','meeting_date','possible_objects','witness_list','visitId'));
+    
+    if(count($visits)>0){
+        foreach($visits as $row){
+            fputcsv($output,$row);
+        }
+    }
+    
+    
+
+ }else if(isset($_POST["detained_data_submit"])){
+    require_once 'dbh.sub.php';
+    require_once 'functions.sub.php';
+ $detained_name=$_POST["detained_name"];
+ if(empty($detained_name)){
+    header("location:../admin_page.php?error6=emptyinput");
+    exit();
+
+}
+if(detainedExists($conn,$detained_name) ==false){
+    header("location:../admin_page.php?error6=detainedNotExist");
+    exit();
+
+}
+     $sql="SELECT * FROM detained WHERE detained_name='".$detained_name."';";
+     if(!$result=mysqli_query($conn,$sql)){
+         exit(mysqli_error($conn));
+     }
+     $detained=array();
+     if(mysqli_num_rows($result)>0){
+     
+         while($row=mysqli_fetch_assoc($result)){
+             $detained[]=$row;
+         }
+     }
+     header('Content-Type:text/csv;charset=utf-8');
+     header('Content-Disposition:attachment; filename=Detained_visits.csv','w');
+     $output=fopen('php://output','w');
+     fputcsv($output,array('detained_name','CNP','imprison-date','release-date','crime','incidents','condemnation','detainedId'));
+     
+     if(count($detained)>0){
+         foreach($detained as $row){
+             fputcsv($output,$row);
+         }
+     }
+     
+    
+
+ }else if(isset($_POST["download_all_detained"])){
+    require_once 'dbh.sub.php';
+   require_once 'functions.sub.php';
+
+    $sql="SELECT * FROM detained;";
+    if(!$result=mysqli_query($conn,$sql)){
+        exit(mysqli_error($conn));
+    }
+    $detained=array();
+    if(mysqli_num_rows($result)>0){
+    
+        while($row=mysqli_fetch_assoc($result)){
+            $detained[]=$row;
+        }
+    }
+    header('Content-Type:text/csv;charset=utf-8');
+    header('Content-Disposition:attachment; filename=Detained.csv','w');
+    $output=fopen('php://output','w');
+    fputcsv($output,array('detained_name','CNP','imprison-date','release-date','crime','incidents','condemnation','detainedId'));
+    
+    if(count($detained)>0){
+        foreach($detained as $row){
+            fputcsv($output,$row);
+        }
+    }
+    
+    
+
+ }
+ 
  else{
      header("location:../admin_page.php");
      exit();
